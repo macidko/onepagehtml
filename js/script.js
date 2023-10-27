1824b8d017d67ff23fc6604c75c0b9ef1512a159
Eren Macit <ernmctt@gmail.com>
2023-10-27 23:49:52 +0300

    css update

--- js/script.js
+++ js/script.js
@@ -2,14 +2,8 @@
 
 // Add event listener to execute code after DOM content loads
 window.addEventListener("DOMContentLoaded", () => {
-
   // Declare variables to be used globally
-  let createLiTag,
-    tagId,
-    tagDetailObj,
-    attrList,
-    allTags,
-    modifiedFrameSrcPath;
+  let createLiTag, tagId, tagDetailObj, attrList, allTags, modifiedFrameSrcPath;
 
   // Get necessary elements from the DOM
   const tagsUl = document.getElementById("tags"), // Ul element for tags
@@ -20,7 +14,9 @@ window.addEventListener("DOMContentLoaded", () => {
     tagCode = document.querySelector(".code"), // Tag code example element
     tagNote = document.querySelector(".note"), // Tag note element
     tagIframe = document.getElementById("iframe"), // Iframe element
-    newIframeSrc = new URL("https://codepen.io/onepagehtml/embed/?default-tab=html%2Cresult"), // Default iframe URL
+    newIframeSrc = new URL(
+      "https://codepen.io/onepagehtml/embed/?default-tab=html%2Cresult"
+    ), // Default iframe URL
     newIframePenSrc = new URL("https://codepen.io/onepagehtml/pen/"), // Default pen URL
     searchInput = document.getElementById("search"); // Search input element
 
@@ -44,7 +40,14 @@ window.addEventListener("DOMContentLoaded", () => {
   }
 
   // Function to change the displayed data in the tag detail section
-  function changeDetailData(tagId, tagHeaderText, tagDescText, tagCodeExample, tagNoteText, iframeSrc) {
+  function changeDetailData(
+    tagId,
+    tagHeaderText,
+    tagDescText,
+    tagCodeExample,
+    tagNoteText,
+    iframeSrc
+  ) {
     setIframeSrc(iframeSrc);
     tagIdAttr.setAttribute("id", tagId);
     tagHeader.textContent = tagHeaderText;
@@ -75,43 +78,48 @@ window.addEventListener("DOMContentLoaded", () => {
       let searchValue = e.target.value.trim().toLowerCase(); // Get search value
 
       // Iterate through all tag elements and handle display
-      Array.from(allTags).forEach(tag => {
+      Array.from(allTags).forEach((tag) => {
         let tagContent = tag.textContent.trim().toLowerCase();
         if (tagContent.includes(searchValue)) {
           tag.style.display = "block"; // Display the tag
         } else {
           tag.style.display = "none"; // Hide the tag
         }
-        
       });
+    });
+  }
+  // Function to show tag details
+  function showTagDetail(index) {
+    createElem("li", tagId[index], "tag", tagId[index], tagsUl);
+
+    createLiTag.addEventListener("click", function (e) {
+      let tagDetailData = tagDetailObj[index]; // Get tag detail data
+      // Update the displayed tag detail information
+      changeDetailData(
+        tagDetailData[0],
+        tagDetailData[1],
+        tagDetailData[2],
+        tagDetailData[3],
+        tagDetailData[4],
+        tagDetailData[6]
+      );
+      closeButton(e); // Handle close button action
     });
   }
 
   // Fetch JSON data
   fetch("../json/data.json")
-    .then(response => response.json())
-    .then(data => {
+    .then((response) => response.json())
+    .then((data) => {
       tagId = data[0]; // Tag IDs
       tagDetailObj = data[1]; // Tag detail objects
       attrList = data[2]; // Attribute lists
 
-      // Function to show tag details
-      function showTagDetail(element, index) {
-        createElem("li", tagId[index], "tag", tagId[index], tagsUl);
-        createLiTag.addEventListener("click", function (e) {
-          let tagDetailData = tagDetailObj[index]; // Get tag detail data
-          // Update the displayed tag detail information
-          changeDetailData(tagDetailData[0], tagDetailData[1], tagDetailData[2], tagDetailData[3], tagDetailData[4], tagDetailData[6]);
-          closeButton(e); // Handle close button action
-        });
-      }
-
       // Iterate through tag IDs and show tag details
       Object.keys(tagId).forEach((element, index) => {
-        showTagDetail(element, index);
+        showTagDetail(index);
       });
 
       search(); // Initialize search functionality
-
     });
 });
